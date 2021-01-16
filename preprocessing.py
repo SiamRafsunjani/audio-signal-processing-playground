136e6851bca21af36de9538d5ec5bc0ebf71ded5
SiamRafsunjani <siamrafsan@gmail.com>
2021-01-16 22:03:07 +0600

    Preprocessing updated

--- preprocessing.py
+++ preprocessing.py
@@ -2,28 +2,35 @@ import json
 import os
 import math
 import librosa
+import pandas as pd
+import sys 
 
-DATASET_PATH = "./Exports"
-JSON_PATH = "data.json"
+DATASET_PATH = "./" + sys.argv[1]
+JSON_PATH = './' + sys.argv[2] + ".json"
+LABEL_PATH = "./description.csv"
 
-def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=1024, num_segments=5):
-    # dictionary to store mapping, labels, and MFCCs
+print(JSON_PATH)
+
+def save_mfcc(dataset_path, json_path, num_mfcc=7, n_fft=2048, hop_length=1024, num_segments=5):
+    # dictionary to store labels, and MFCCs
     data = {
-        "mapping": [],
-        "labels": [],
-        "mfcc": []
+        "mfcc": [],
+        "labels": []
     }
 
-    samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
-    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+    labels = pd.read_csv(LABEL_PATH)
+    
+    print(labels.loc[labels['name'] == 'lwmxjcxc.mp3'])
+
     counter = 0
     for root, dirs, files in os.walk(dataset_path):
         if root is dataset_path:
-            for file in files:
-                if counter == 10: break
+            for file in files:                
                 counter = counter + 1
+                # if counter is 2: break 
+
                 file_path = os.path.join(root, file)
-                signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+                signal, sample_rate = librosa.load(file_path, sr=None)
                 mfcc = librosa.feature.mfcc(signal, sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
                 mfcc = mfcc.T
 
@@ -31,12 +38,12 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=1024,
                 print(file)
                 print(len(mfcc))
 
-                data["mfcc"].append(mfcc.tolist())
-                # data["labels"].append(i-1)
+                data["mfcc"].append([mfcc.tolist()])
+                data["labels"].append(int( labels.loc[labels['name'] == file]['class'] ) )
+
 
     # save MFCCs to json file
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
-        
         
 save_mfcc(DATASET_PATH, JSON_PATH, num_segments=1)
